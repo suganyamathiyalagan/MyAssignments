6f1ac34501cfe95ecb958189adb5532b68434da5
suganyamathiyalagan <suganyamathiyalagan02@gmail.com>
2026-07-21 15:39:44 +0530

    Add files via upload
    day 01

---
+++ day01.js
@@ -0,0 +1,12 @@
+var firstName = "suganya"
+console.log(firstName)
+var companyName = "HCL"
+console.log(companyName)
+var mobilenumber=8760171262
+console.log(mobilenumber)
+var isAutomation 
+isAutomation= true
+console.log(isAutomation)
+var hasplaywright
+console.log(hasplaywright)
+console.log(typeof firstName,typeof companyName,typeof mobilenumber,typeof isAutomation,typeof hasplaywright)
