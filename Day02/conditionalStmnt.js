10df20518df79c4853a2d9e53ac413db0aeebe21
suganyamathiyalagan <suganyamathiyalagan02@gmail.com>
2026-07-22 13:42:00 +0530

    Add files via upload

---
+++ Day02/conditionalStmnt.js
@@ -0,0 +1,10 @@
+function launchbrowser(){
+    let browserName = "safari";
+    if(browserName == "chrome"){
+        console.log("the browser name is chrome");
+    }
+    else{
+        console.log("other browser such as firefox,safari");
+    }
+}
+launchbrowser();
---
+++ Day02/numberType.js
@@ -0,0 +1,15 @@
+function numberType() {
+   let num = 0;
+
+if (num>0){
+    console.log( num + "is greater than 0 and is a positive number");
+
+}
+else if (num<0){
+    console.log( num + "is less then 0 and is a negative number");
+}
+else {
+    console.log("this is a neutral number");
+}
+}
+numberType();
