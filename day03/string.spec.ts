275b38af6ca708c81632a45c271f7c341c2250f5
suganyamathiyalagan <suganyamathiyalagan02@gmail.com>
2026-08-09 17:29:23 +0530

    Add files via upload

---
+++ day03/string.spec.ts
@@ -0,0 +1,5 @@
+let s = "Hello World"
+const strARR = s.split(" ")
+const lastword =strARR[strARR.length-1] 
+console.log('the last word is ${lastword}')
+console.log('lenght of the last word is ${lastword.length}')
